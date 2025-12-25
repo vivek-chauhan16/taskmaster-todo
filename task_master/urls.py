643b0dfc071f4e1dff93a337504cc945dd048ba6
VIVEK CHAUHAN <chauhanvivek1714@gmail.com>
2025-12-25 18:36:25 +0530

    all functions working with improved ui

--- task_master/urls.py
+++ task_master/urls.py
@@ -3,5 +3,14 @@ from . import views
 
 urlpatterns = [
      path('addTask/', views.addTask, name='addTask'),
-     
+
+     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+     path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+
+     path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+
+     path('edit_task/<int:pk>/', views.edit_task, name="edit_task"),
+
+  
 ]
--- task_master/views.py
+++ task_master/views.py
@@ -1,8 +1,49 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from .models import Task
 
 # Create your views here.
 def addTask(request):
-    task = request.POST['task']
+    task = request.POST.get('task','').strip()
+    if task == "":
+        messages.error(request, "❌ Task cannot be empty!", extra_tags='add_error')
+        return redirect('home')
     Task.objects.create(task=task)
     return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request,pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('home')
+
+def edit_task(request,pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        new_task = request.POST.get('task', '').strip()
+
+        if new_task == "":
+            messages.error(request,"❌ Task cannot be empty!")
+            return redirect("edit_task",pk=pk)
+
+        get_task.task = new_task
+        get_task.save()
+        messages.success(request, "✅ Task updated successfully!", extra_tags='update_success')
+        return redirect('home')
+    else:
+        context={
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
+    
